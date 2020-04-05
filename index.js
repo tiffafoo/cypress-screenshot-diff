77cb6f8617a183d19ee8c276f8c7d13b36403d34
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2020-04-05 19:43:57 -0400

    refactor: export for fs issue

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { addCommands } = require('./src/commands');
 
 const {
+  baseExists,
+  compareScreenshots,
+  onAfterScreenshot,
   addScreenshotDiffPlugin,
 } = require('./src/plugins');
 
 module.exports = {
   addCommands,
+  baseExists,
+  compareScreenshots,
+  onAfterScreenshot,
   addScreenshotDiffPlugin,
 };
